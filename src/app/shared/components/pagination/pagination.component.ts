b09c884570455dfd8eb28a2c778a0968d02529aa
ahmed-yasser12 <ahmedyasser541@gmail.com>
2026-05-13 23:10:35 +0300

    3 reusable component are done

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, input, output, Pipe } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss'
 })
 export class PaginationComponent {
+
+  currentPage = input<number>(1);
+  totalPages = input<number[]>([1, 2, 3, 4, 5]);
+  showPrevNext = input<boolean>(true);
+  pageChange = output<number>();
+
+
+  selectPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages().length && page !== this.currentPage()) {
+      this.pageChange.emit(page);
+    }
+  }
+
+  pages(): number[] {
+    return Array.from(
+      { length: this.totalPages().length },
+      (_, index) => index + 1
+    );
+
+  }
 
 }
